d56bf533f5de4807c6cf730c70516d691cabb0f7
Max Zangs <maximilian.zangs@gmail.com>
2024-09-15 16:37:51 +0200

    Reordering test

--- src/test/01/main.py
+++ src/test/01/main.py
@@ -1,21 +1,18 @@
 import os
 
-from ..my_draw import Plotter
-from ..my_draw import Parser
+from ...my_draw import Plotter
+from ...my_draw import Parser
 
 
 PORT = os.getenv("PLOTTER_PORT")
 if PORT is None:
     raise Exception("Please proveide the serial port of the plotter")
 
-TEST_SVG = os.getenv("PLOTTER_TEST_SVG")
-if TEST_SVG is None:
-    raise Exception("Please proveide a testing SVG")
-
-parser = Parser.from_file(TEST_SVG)
+parser = Parser.from_file("src/test/01/a5.svg")
+parser.interpolate()
 parser.optimize_curves()
+parser.scale_to_fit("a6")
 
 plotter = Plotter(PORT)
-parser.scale_to_fit((135, 200))
 plotter.convert_curves(parser.curves)
 plotter.exec_commands()
